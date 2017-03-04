fd86819cc9a9a7fcdb04949600173e4e4dc556fc
David Chan <davidkwokhochan@gmail.com>
2017-03-03 16:09:52 -0800

    Added Box Service Class and Spec

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var BoxSDK = require('box-node-sdk');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,32 +12,6 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
-// Initialize SDK
-var sdk = new BoxSDK({
-  clientID: 'dab95mxwvro2hniqxj5tw693celgmoee',
-  clientSecret: '5243kaAZJzlnlzjKyRkE1ZWbfs22H0L0'
-});
-
-// Create a basic API client
-// Creating and Generating a Developer Token, expires every 60 mins
-var client = sdk.getBasicClient('IdSqAIHSUCkD7J67hVsVR1FjWtkrODre');
-
-// Get some of that sweet, sweet data!
-client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
-  if(err) throw err;
-  console.log('Hello, ' + currentUser.name + '!');
-});
-
-client.folders.getItems( '0', {
-        fields: 'name,modified_at,size,url,permissions,sync_state',
-        offset: 0,
-        limit: 25 },
-        function(err, data) {
-          if(err) throw err;
-          console.log('data is' + data,keys());
-        }
-);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ services/boxService.js
@@ -0,0 +1,58 @@
+export class BoxService{
+
+  var developerToken = 'IdSqAIHSUCkD7J67hVsVR1FjWtkrODre'
+
+  setUp(){
+    var BoxSDK = require('box-node-sdk');
+    // Initialize SDK
+    var sdk = new BoxSDK({
+      clientID: 'dab95mxwvro2hniqxj5tw693celgmoee',
+      clientSecret: '5243kaAZJzlnlzjKyRkE1ZWbfs22H0L0'
+    });
+    // Create a basic API client
+    // Creating and Generating a Developer Token, expires every 60 mins
+    var client = sdk.getBasicClient(developerToken);
+    // Get some of that sweet, sweet data!
+  }
+
+  getUserName(){
+    client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
+      if(err) throw err;
+      console.log('Hello, ' + currentUser.name + '!');
+    });
+  }
+
+  getAllItemsInRootFolder(callBack){
+    client.folders.getItems( '0', {
+            fields: 'name,modified_at,size,url,permissions,sync_state',
+            offset: 0,
+            limit: 25 }, callBack
+    );
+  }
+
+  getCategory(){
+
+  }
+
+  getSubCategory(){
+
+  }
+
+  getSubCategory(){
+
+  }
+
+}
+// angular.module('boxService', [])
+//   .service('sampleSvc', ['$window', 'modalSvc', function($window, modalSvc){
+//     this.showDialog = function(message, title){
+//       if(title){
+//         modalSvc.showModalDialog({
+//           title: title,
+//           message: message
+//         });
+//       } else {
+//         $window.alert(message);
+//       }
+//     };
+//   }]);
